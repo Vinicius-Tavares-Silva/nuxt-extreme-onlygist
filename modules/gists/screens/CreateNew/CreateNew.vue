eca8e3a2c3994bc2175c0533822e1df10ddbe265
Vinícius Tavares da Silva <82236212+Vinicius-Tavares-Silva@users.noreply.github.com>
2024-11-26 23:06:19 +0000

    feat: create gist creation

---
+++ modules/gists/screens/CreateNew/CreateNew.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HeadlineEdit from '@/modules/gists/components/HeadlineEdit/HeadlineEdit.vue'
+import { myselfKey } from '@/modules/users/composables/useMyself/useMyself'
+import type { MyselfContextProvider } from '@/modules/users/composables/useMyself/types'
+
+const { user, loading } = inject(myselfKey) as MyselfContextProvider
+
+</script>
+
+<template>
+  <WidgetDefault title="Qual gist você quer criar?" class="my-5">
+    <HeadlineEdit />
+  </WidgetDefault>
+</template>
---
+++ pages/app/gist/create.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import CreateNew from '@/modules/gists/screens/CreateNew/CreateNew.vue'
+
+definePageMeta({
+  layout: 'admin',
+  middleware: ['auth'],
+})
+
+</script>
+
+<template>
+  <CreateNew />
+</template>
